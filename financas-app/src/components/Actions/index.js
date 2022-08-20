9d4fdb99d272201f6178c8cf82aa25e9a522ccd1
Henrique <lhenrique.lh000@gmail.com>
2022-08-20 16:11:45 -0300

    Telas transferencia

--- financas-app/src/components/Actions/index.js
+++ financas-app/src/components/Actions/index.js
@@ -1,34 +1,37 @@
 import React from 'react';
 import { View, Text, TouchableOpacity, ScrollView } from 'react-native';
 import {AntDesign, Foundation} from '@expo/vector-icons'
+import { useNavigation } from '@react-navigation/native'
 import styles from './styles'
 
 export default function Actions() {
+
+const navigation = useNavigation();
+
  return (
     <ScrollView style={styles.container} horizontal={true} showsHorizontalScrollIndicator={false}>
-        <TouchableOpacity styles={styles.actionButton}> 
+        <TouchableOpacity 
+        style={styles.actionButton}
+        onPress={ () => navigation.navigate('SelectPaymentAccount')}
+        > 
             <View style={styles.areaButton}>
                 <Foundation name="dollar-bill" size={26} color="#000"/>
             </View>
             <Text style={styles.labelButton}>Realizar</Text>
-            <Text>pagamento</Text>
+            <Text style={styles.labelButton}>pagamento</Text>
         </TouchableOpacity>
 
         <TouchableOpacity style={styles.actionButton}> 
             <View style={styles.areaButton}>
-                <AntDesign name="creditcard" size={26} color="#000"/>
+                <AntDesign name="copy1" size={26} color="#000"/>
             </View>
-            <Text style={styles.labelButton}>Carteira</Text>
+            <Text style={styles.labelButton}>Extrato</Text>
         </TouchableOpacity> 
 
-        <TouchableOpacity style={styles.actionButton}> 
-            <View style={styles.areaButton}>
-                <AntDesign name="barcode" size={26} color="#000"/>
-            </View>
-            <Text style={styles.labelButton}>Boletos</Text>
-        </TouchableOpacity> 
-
-        <TouchableOpacity style={styles.actionButton}> 
+        <TouchableOpacity 
+        style={styles.actionButton}
+        onPress={ () => navigation.navigate('Profile')}
+        > 
             <View style={styles.areaButton}>
                 <AntDesign name="user" size={26} color="#000"/>
             </View>
---
+++ financas-app/src/pages/SelectPaymentAccount/styles.js
@@ -0,0 +1,33 @@
+import {StyleSheet} from 'react-native';
+
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fafafa'
+    },
+
+    containerTitle: {
+      marginBottom: '7%', 
+      alignSelf: 'center'
+  },
+  
+    title:{
+      fontSize: 28,
+      fontWeight: 'bold',
+      margin: 14
+  },
+   
+  containerHeader:{
+    width: '80%',
+    marginTop: '12%'
+  },
+
+  buttonBack:{
+    marginStart: 15,
+    width: 40
+  },
+
+  });
+
+export default styles;
