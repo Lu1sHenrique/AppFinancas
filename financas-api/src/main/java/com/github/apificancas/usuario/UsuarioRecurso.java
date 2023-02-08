692936f1abfdba45363ca7c96ad20d47c052e694
elbertribeiro <elbertprofissional@gmail.com>
2023-02-08 16:09:09 -0300

    refactory: refatorando controller criando arquivo de Configuracao

--- financas-api/src/main/java/com/github/apificancas/usuario/UsuarioRecurso.java
+++ financas-api/src/main/java/com/github/apificancas/usuario/UsuarioRecurso.java
@@ -1,12 +1,7 @@
 package com.github.apificancas.usuario;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.security.web.authentication.HttpStatusEntryPoint;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,29 +11,10 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/user")
-public class UsuarioRecurso extends WebSecurityConfigurerAdapter {
+public class UsuarioRecurso {
 
     @GetMapping
     public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
         return Collections.singletonMap("name", principal.getAttribute("name"));
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests(a -> a
-                        .antMatchers("/", "/error", "/webjars/**").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .exceptionHandling(e -> e
-                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
-                )
-                .csrf(c -> c
-                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-                )
-                .logout(l -> l
-                        .logoutSuccessUrl("/").permitAll()
-                )
-                .oauth2Login();
     }
 }
