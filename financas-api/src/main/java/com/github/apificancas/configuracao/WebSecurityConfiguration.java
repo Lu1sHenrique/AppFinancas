a6886142a4c65f6bc546a69afdf104893490f41b
elbertribeiro <elbertprofissional@gmail.com>
2023-02-08 16:21:13 -0300

    feat: adicionando configuração para usar o swagger sem auth

--- financas-api/src/main/java/com/github/apificancas/configuracao/WebSecurityConfiguration.java
+++ financas-api/src/main/java/com/github/apificancas/configuracao/WebSecurityConfiguration.java
@@ -9,11 +9,21 @@ import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private static final String[] AUTH_WHITELIST = {
+            "/",
+            "/error",
+            "/webjars/**",
+            "/v3/api-docs/**",
+            "/swagger-ui/**",
+            "/swagger-ui.html",
+    };
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests(a -> a
-                        .antMatchers("/", "/error", "/webjars/**").permitAll()
+                        .antMatchers(AUTH_WHITELIST).permitAll()
                         .anyRequest().authenticated()
                 )
                 .exceptionHandling(e -> e
