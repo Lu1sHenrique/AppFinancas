4304bca6715bf8404b0bd3c116ee5fd6a2a675ec
elbertribeiro <elbertprofissional@gmail.com>
2023-02-08 16:05:35 -0300

    refactory: refatorando controller

--- financas-api/src/main/java/com/github/apificancas/usuario/UsuarioRecurso.java
+++ financas-api/src/main/java/com/github/apificancas/usuario/UsuarioRecurso.java
@@ -7,6 +7,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,9 +15,10 @@ import java.util.Collections;
 import java.util.Map;
 
 @RestController
+@RequestMapping("/user")
 public class UsuarioRecurso extends WebSecurityConfigurerAdapter {
 
-    @RequestMapping("/user")
+    @GetMapping
     public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
         return Collections.singletonMap("name", principal.getAttribute("name"));
     }
