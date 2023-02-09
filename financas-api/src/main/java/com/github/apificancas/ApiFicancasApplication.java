b95ede241be7342a049cf24b07f127702e583736
elbertribeiro <elbertprofissional@gmail.com>
2023-02-09 20:36:19 -0300

    revert: adicionando titulo no swagger

--- financas-api/src/main/java/com/github/apificancas/ApiFicancasApplication.java
+++ financas-api/src/main/java/com/github/apificancas/ApiFicancasApplication.java
@@ -1,11 +1,8 @@
 package com.github.apificancas;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@OpenAPIDefinition(info=@Info(title="Financas API"))
 @SpringBootApplication
 public class ApiFicancasApplication {
     public static void main(String[] args) {
